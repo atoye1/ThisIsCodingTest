f56328ca382d003a8938ca365c9065d96f1a18cb
Seol Donghun <ssalssi1@gmail.com>
2022-06-21 22:52:28 +0900

    chapter 03 Greedy solved

---
+++ Greedy/3.4_until_one.py
@@ -0,0 +1,13 @@
+N, K = map(int, input().split())
+count = 0
+
+while True:
+    target = (N // K) * K
+    count += N - target
+    N = target
+    if N < K:
+        break
+    count += 1
+    N //= K
+count += N - 1
+print(count)
